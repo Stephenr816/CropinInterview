1c4aa630401efae1fc57b7136135c83985bbb043
Stephenr816 <stephenr816@gmail.com>
2022-04-18 11:25:14 +0530

    LoginPage Test added

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -1,10 +1,49 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginPage {
+	
+	WebDriver driver;
+	
+	//1. Private Locators
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private By username = By.id("input-email");
+	private By password = By.id("input-password");
+	private By loginButton = By.xpath("//input[@value='Login']");
+	private By forgotPwdLink = By.xpath("//div[@class='form-group']//a[text()='Forgotten Password']");
+	
+	//2. Constructor
+	
+	public LoginPage(WebDriver driver) {
+		
+		this.driver= driver;
 	}
-
+	
+	//3. Public page actions (methods)
+	
+	public String getLoginPageTitle() {
+		
+		return driver.getTitle();
+	}
+	
+	public String getLoginPageUrl() {
+		
+		return driver.getCurrentUrl();
+	}
+	
+	public boolean isForgotPwdLinkExist() {
+		
+		return driver.findElement(forgotPwdLink).isDisplayed();
+	}
+	
+	public void doLogin(String un, String pwd) {
+		
+		driver.findElement(username).sendKeys("stephenr816@gmail.com");
+		driver.findElement(password).sendKeys("stephen@123");
+		driver.findElement(loginButton).click();
+	}
+   
 }
--- src/test/java/com/qa/opencart/test/LoginPageTest.java
+++ src/test/java/com/qa/opencart/test/LoginPageTest.java
@@ -1,9 +1,48 @@
 package com.qa.opencart.test;
 
-public class LoginPageTest {
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+import com.qa.opencart.factory.DriverFactory;
+import com.qa.opencart.pages.LoginPage;
+
+public class LoginPageTest {
+	
+	WebDriver driver;
+	LoginPage lp;
+
+	@BeforeTest
+	public void setUp() {
+		
+		DriverFactory df = new DriverFactory();
+		driver = df.init_driver("chrome");
+		lp=new LoginPage(driver);
+		
+	}
+	
+	@Test(priority = 1)
+	public void loginPageTitleTest() {
+		String title = lp.getLoginPageTitle();
+		Assert.assertEquals(title, "Account Login");
+	}
+	
+	@Test(priority = 2)
+	public void loginPageUrlTest() {
+		String url = lp.getLoginPageUrl();
+		Assert.assertTrue(url.contains("account/login"));
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+	 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
